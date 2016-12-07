7d5e5cff6e997dd242df83edcf6010cafed0b327
Arman <arman@innorise.com>
2016-12-07 12:19:43 +0400

    Sign in custom classes complite

---
+++ SocialNetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialNetwork
+//
+//  Created by Arman on 12/7/16.
+//  Copyright © 2016 InnorieseEntertainment. All rights reserved.
+//
+
+import UIKit
+
+var SHADOW_GREY : CGFloat = 125 / 250
---
+++ SocialNetwork/FancyBtn.swift
@@ -0,0 +1,23 @@
+//
+//  FancyBtn.swift
+//  SocialNetwork
+//
+//  Created by Arman on 12/7/16.
+//  Copyright © 2016 InnorieseEntertainment. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor (red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+    
+}
